bd8fd91dcea2b274f858e10ab86fa583ef2f629a
Jose Nilton <jnorejuela@hotmail.com>
2024-03-07 02:34:01 -0500

    Add class and inheritence

---
+++ class_and_inheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿// algoritmo para crear la bases de un rectangulo.
+//clase
+/*Console.WriteLine("Hello, World!");
+var rectan = new rectangulo();
+rectan.Base = 20;
+rectan.Altura = 20;
+Console.WriteLine($"El area es: {rectan.Area()}");
+Console.WriteLine($"El periometro es: {rectan.Perimetro()}");
+*/
+//end classe
+var cuadra = new Cuadrado();
+cuadra.Base = 20;
+cuadra.Altura = 20;
+Console.WriteLine($"El area es: {cuadra.Area()}");
+Console.WriteLine($"El periometro es: {cuadra.Perimetro()}");
+Console.WriteLine($"El lado es: {cuadra.Lado}");
+
+class Rectangulo{
+    public double Base{get; set;}
+    public double Altura{get; set;}
+    public double Area() => Base*Altura;
+    public double Perimetro() => 2* Base + 2 * Altura;
+}
+
+//herencia
+class Cuadrado : Rectangulo{
+    public double Lado{
+        set { Base = value; Altura = value;}
+        get {return Base; }
+
+    }
+
+}
+
+
