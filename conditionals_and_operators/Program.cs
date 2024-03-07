1ddcf6f26fdf5a70e56037f05e6d71ac2d18917e
Jose Nilton <jnorejuela@hotmail.com>
2024-03-07 01:23:39 -0500

    Conditionals and Operators

---
+++ conditionals_and_operators/Program.cs
@@ -0,0 +1,16 @@
+﻿/* condicionales
+// crear un programa que muestre si el numero es negativo o positivo.
+ Console.WriteLine($"Ingrese un numero:");
+ int num = int.Parse(Console.ReadLine());
+ if (num > 0) Console.WriteLine("Positivo");
+ else if(num < 0) Console.WriteLine("Negativo");
+ else Console.WriteLine("Es cero");
+ */
+
+/* operadores*/
+Console.WriteLine("Ingrese un numero:");
+double numero = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingresa otro numero:");
+double numero1 = double.Parse(Console.ReadLine());
+const string Indeterminado = "Indeterminado";
+Console.WriteLine($"Suma: {numero + numero1}, Resta: {numero - numero1}, Multiplicacion: {numero * numero1}, Division: {(numero1 != 0 ? numero / numero1 : Indeterminado)}");
